bc5e527817817154d211dfa816d5464308655b8b
zhouzexu481 <zhouzexu481@gmail.com>
2025-12-18 21:47:52 +0800

    2

--- Input/BH1750.c
+++ Input/BH1750.c
@@ -3,20 +3,20 @@
 #include "MyI2C2.h"
 #include "Delay.h"
 #include <math.h>
-//写入初始化命令(上电,连续测量模式),读取数据命令
 
+// 写初始化函数(上电, 设置测量模式), 读取光照数据
 void BH1750_Init(void)
 {
     MyI2C2_Start();
-    MyI2C2_SendByte(BH1750_ADDRESS_WRITE);		//写数据
+    MyI2C2_SendByte(BH1750_ADDRESS_WRITE);      // 写入地址
     MyI2C2_ReceiveAck();
-    MyI2C2_SendByte(BH1750_POWER_ON);			//写入数据上电
+    MyI2C2_SendByte(BH1750_POWER_ON);           // 写入指令：上电
     MyI2C2_ReceiveAck();
     MyI2C2_Stop();
     
     Delay_ms(10);
     
-    // 1. 在初始化时就启动连续测量
+    // 1. 在初始化时设置测量模式
     MyI2C2_Start();
     MyI2C2_SendByte(BH1750_ADDRESS_WRITE);
     MyI2C2_ReceiveAck();
@@ -24,7 +24,7 @@ void BH1750_Init(void)
     MyI2C2_ReceiveAck();
     MyI2C2_Stop();
     
-    // 给传感器第一次测量的时间，防止刚上电马上读数据读到0
+    // 延时等待第一次测量时间，防止上电直接读取数据读到0
     Delay_ms(180); 
 }
 
@@ -34,20 +34,20 @@ float BH1750_ReadLightLevel(void)
     uint16_t raw_data;
     float lux;
     
-    // 直接读取寄存器中最新的值
-	//BH1750输出的是16位数据
+    // 直接读取传感器内的光照值
+    // BH1750返回的是16位数据
     MyI2C2_Start();
-    MyI2C2_SendByte(BH1750_ADDRESS_READ);		//读数据
+    MyI2C2_SendByte(BH1750_ADDRESS_READ);       // 读取地址
     MyI2C2_ReceiveAck();
     
     data[0] = MyI2C2_ReceiveByte();
-    MyI2C2_SendAck(0); // 0通常代表ACK（继续读）
+    MyI2C2_SendAck(0); // 0: 发送ACK, 继续读取
     data[1] = MyI2C2_ReceiveByte();
-    MyI2C2_SendAck(1); // 1通常代表NACK（停止读）
+    MyI2C2_SendAck(1); // 1: 发送NACK, 停止读取
     MyI2C2_Stop();
     
     raw_data = (data[0] << 8) | data[1];
-    lux = raw_data / 1.2;	//1.2 是 BH1750 传感器的分辨率
+    lux = raw_data / 1.2;   // 1.2 是 BH1750 的测量分辨率系数
     
     return lux;
 }
